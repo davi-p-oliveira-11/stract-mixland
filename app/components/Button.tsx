9e9d7b4d7357fd13c691d3b0d15d7b8749282d75
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-10-27 16:17:54 -0300

    added button component

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { FC, ButtonHTMLAttributes } from "react";
+import clsx from "clsx";
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary";
+  size?: "sm" | "md" | "lg";
+  fullWidth: boolean;
 }
 
-export default Button
+const Button: FC<ButtonProps> = ({
+  variant = "primary",
+  size = "md",
+  fullWidth = false,
+  className,
+  ...props
+}) => {
+  const base = "font-semibold transition cursor-pointer";
+
+  const variants = {
+    primary: "bg-[#2B59FF] text-white hover:bg-[#2B59FF]",
+    secondary: "bg-gray-900 text-black hover:bg-gray-900",
+  };
+
+  const sizeClasses = {
+    sm: "px-4 py-2 text-sm rounded-md",
+    md: "w-[120px] h-[45px] text-base rounded-full sm:w-[140px] sm:h-[50px] md:w-[160px] md:h-[60px]",
+    lg: "px-8 py-4 text-lg rounded-full",
+  };
+
+  return (
+    <button
+      className={clsx(
+        base,
+        variants[variant],
+        sizeClasses[size],
+        fullWidth && "w-full",
+        className
+      )}
+      {...props}
+    />
+  );
+};
+
+export default Button;
